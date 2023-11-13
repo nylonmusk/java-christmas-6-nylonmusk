351dedb52d44828bf3d3abe4b5672b6f7385b039
nylonmusk <2912rla@gmail.com>
2023-11-14 03:23:59 +0900

    refactor : NOTHING 상수, getName 메소드 추가

--- src/main/java/christmas/constant/badge/Badge.java
+++ src/main/java/christmas/constant/badge/Badge.java
@@ -1,10 +1,11 @@
-package christmas.constant;
+package christmas.constant.badge;
 
 public enum Badge {
     SANTA("산타", 20000),
     TREE("트리", 10000),
     STAR("별", 5000),
-    NONE("없음", 0);
+    NONE("없음", 0),
+    NOTHING("", 0);
 
     private final String name;
     private final int qualifyingAmount;
@@ -18,9 +19,13 @@ public enum Badge {
         return this.qualifyingAmount;
     }
 
-    public static String getBadgeByAmount(int amount) {
+    public String getName() {
+        return name;
+    }
+
+    public static String getBadgeByAmount(int totalDiscounts) {
         for (Badge badge : values()) {
-            if (amount >= badge.getQualifyingAmount()) {
+            if (totalDiscounts >= badge.getQualifyingAmount()) {
                 return badge.name;
             }
         }
