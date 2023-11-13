9f26f3065eecb153be2f65b1a77f7df3558c70c4
nylonmusk <2912rla@gmail.com>
2023-11-14 03:18:47 +0900

    refactor : NO_COMPLIMENTARY 추가

--- src/main/java/christmas/constant/complimentary/ComplimentaryDescription.java
+++ src/main/java/christmas/constant/complimentary/ComplimentaryDescription.java
@@ -1,7 +1,9 @@
 package christmas.constant.complimentary;
 
 public enum ComplimentaryDescription {
-    COMPLIMENTARY_DISCOUNT("증정 이벤트: -");
+    COMPLIMENTARY_DISCOUNT("증정 이벤트: -"),
+    NO_COMPLIMENTARY("없음\n");
+
 
     private final String description;
 
