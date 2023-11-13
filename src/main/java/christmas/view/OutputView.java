a74e7bffd8dfcd29389d72e170bf67b4fbc6043e
nylonmusk <2912rla@gmail.com>
2023-11-13 17:33:19 +0900

    feat : 출력을 하는 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,74 @@
+package christmas.view;
+
+import christmas.util.FormatCurrencyUtil;
+
+public class OutputView {
+    public OutputView() {
+    }
+
+    public void printWelcomeMessage() {
+        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void askForRestaurantVisitDate() {
+        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
+    public void requestMenuOrder() {
+        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
+
+    public void printPreviewOfEventBenefits() {
+        println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        println("");
+    }
+
+    public void printOrder(String formattedOrderList) {
+        println("<주문 메뉴>");
+        println(formattedOrderList);
+
+    }
+
+    public void printTotalOrderAmountBeforeDiscount(int totalPriceBeforeDiscount) {
+        println("<할인 전 총주문 금액>");
+        println(FormatCurrencyUtil.formatCurrency(totalPriceBeforeDiscount));
+    }
+
+    public void printComplimentaryItems(String complimentaryItems) {
+        println("<증정 메뉴>");
+        println(complimentaryItems);
+    }
+
+    public void printDetailBenefits(String discountInformation){
+        println("<혜택 내역>");
+        println(discountInformation);
+    }
+
+    public void printTotalDiscounts(int totalDiscounts) {
+        println("<총혜택 금액>");
+        if (totalDiscounts > 0) print("-");
+        println(FormatCurrencyUtil.formatCurrency(totalDiscounts));
+    }
+
+    public void printFinalOrderAmount (int finalPrice){
+        println("<할인 후 예상 결제 금액>");
+        println(FormatCurrencyUtil.formatCurrency(finalPrice));
+    }
+
+    public void printBadge (String badge){
+        println("<12월 이벤트 배지>");
+        println(badge);
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+
+    private void print(String message) {
+        System.out.print(message);
+    }
+}
