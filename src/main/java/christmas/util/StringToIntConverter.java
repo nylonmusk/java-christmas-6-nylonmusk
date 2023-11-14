27181bbd5514a4597a96c7717ea0608be09067a6
nylonmusk <2912rla@gmail.com>
2023-11-14 18:41:26 +0900

    refactor : final 선언

--- src/main/java/christmas/util/StringToIntConverter.java
+++ src/main/java/christmas/util/StringToIntConverter.java
@@ -1,6 +1,9 @@
 package christmas.util;
 
-public class StringToIntConverter {
+public final class StringToIntConverter {
+
+    private StringToIntConverter() {
+    }
 
     public static int parseInt(String text) {
         return Integer.parseInt(text);
